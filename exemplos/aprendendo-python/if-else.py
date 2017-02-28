5dcd986d8123f769dbb8cd56d618d45d18e71cc0
Marianne Linhares Monteiro <marianne.monteiro@ccc.ufcg.edu.br>
2017-02-27 21:48:09 -0300

    Update if-else.py

--- exemplos/aprendendo-python/if-else.py
+++ exemplos/aprendendo-python/if-else.py
@@ -1,4 +1,5 @@
-numero = int(raw_input()) #transforma o que se recebe em um inteiro
+numero = int(raw_input()) #converte o que se recebe em um inteiro
+
 if numero > 0:
 	print "eh positivo"
 elif numero < 0:
